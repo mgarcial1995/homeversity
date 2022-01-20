9f8ca42a54afee9fe530aee3c1870f1d81c2e949
Piero Loza <pieroloza1202@gmail.com>
2022-01-20 17:17:23 -0500

    feat(react-homeversity): add Increase component

--- packages/react-homeversity/src/App.js
+++ packages/react-homeversity/src/App.js
@@ -1,18 +1,19 @@
-import Home from './views/Home';
-import Carshop from './views/Carshop';
-import Premium from './views/Premium';
-import Programs from './views/Programs';
+import Home from "./views/Home";
+import Carshop from "./views/Carshop";
+import Premium from "./views/Premium";
+import Programs from "./views/Programs";
+import Increase from "./components/Increase";
 import { Routes, Route } from "react-router-dom";
-import './App.css';
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/programs' element={<Programs/>} />
-        <Route path='/premium' element={<Premium/>} />
-        <Route path='/carshop' element={<Carshop/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/premium" element={<Premium />} />
+        <Route path="/carshop" element={<Carshop />} />
       </Routes>
     </div>
   );
