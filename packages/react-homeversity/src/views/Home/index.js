f75dd1a6ca1c9155a9958ea04d55ccbfc9d1791c
Martin <martingarcial.1995@gmail.com>
2022-01-20 18:59:53 -0500

    feat: merge branch

--- packages/react-homeversity/src/views/Home/index.js
+++ packages/react-homeversity/src/views/Home/index.js
@@ -1,51 +1,29 @@
-import Mentores from '../../components/Mentores'
-import Header from '../../components/Header';
+import Mentores from "../../components/Mentores";
+import Header from "../../components/Header";
 import Increase from "../../components/Increase";
 import courses_home from "../../courses_home";
 import React from "react";
 import Courses from "../../components/Courses";
-import "./styles.sass"
+import "./styles.sass";
 
 function Home() {
   return (
     <div>
-      {/* NAVBAR */}
+      <Header />
+      <main className='box1'>
+        <div className='box2'>
+          {" "}
+          <strong>NUESTROS CURSOS</strong>
+        </div>
 
-      {/* NAVBAR */}
-
-    {/* HEADER */}
-    <Header/>
-    {/* HEADER */}
-
-      {/* NUESTROS CURSOS */}
-      <main className="box1">
-       
-        
-       <div className="box2"> <strong>NUESTROS CURSOS</strong></div>
-      
-       <section className="courses" id="courses">
-      
-         {courses_home.map((elem) => {
-           
-           return <Courses key = {elem.id} data={elem} />
-         
-         })
-         }
-       </section>
-       </main>
-      {/* NUESTROS CURSOS */}
-
-      {/* CRECIMIENTO PROFESIONAL */}
+        <section className='courses' id='courses'>
+          {courses_home.map((elem) => {
+            return <Courses key={elem.id} data={elem} />;
+          })}
+        </section>
+      </main>
       <Increase />
-      {/* CRECIMIENTO PROFESIONAL */}
-
-      {/* APRENDE DE LA MANO */}
-
-      {/* APRENDE DE LA MANO */}
-
-    <Mentores />
-
-      {/* FOOTER */}
+      <Mentores />
     </div>
   );
 }
