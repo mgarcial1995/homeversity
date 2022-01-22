b7937aca629d01d6c485d556540943143cf4a962
jfernand <jfernand196@gmail.com>
2022-01-21 21:49:52 -0500

    update: Component Courses

--- packages/react-homeversity/src/courses_home.js
+++ packages/react-homeversity/src/courses_home.js
@@ -1,256 +1,77 @@
 const courses_home = [
     {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
+      "img": "https://www.mundoenred.com/img/news/picture_8677.jpg",
       "id": "61e7673ed9e9c0e1e1ba2e72",
       "teacher": "Hernandez Daniel",
-      "name": "enim magna aliquip",
+      "name": "Carpinteria",
       "about": "Dolor incididunt est aliqua voluptate esse.",
-      "price": 20.56,
+      "price": "S/. 20.56",
       "stars": 4
     },
     { 
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
+      "img": "https://d2qc4bb64nav1a.cloudfront.net/cdn/13/images/curso-online-electricidad-y-electronica_l_primaria_1_1527681570.jpg",
       "id": "61e7673e6a593d9d99441227",
       "teacher": "Delia Quinn",
-      "name": "velit pariatur nulla",
-      "about": "Cillum ad commodo reprehenderit dolor irure proident culpa.",
-      "price": 16.74,
+      "name": "Electricidad",
+      "about": "Cillum ad commodo reprehenderit dolor irure proident.",
+      "price": "S/. 16.74",
       "stars": 2
     },
     {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
+      "img": "https://www.salirconarte.com/wp-content/uploads/2018/10/2266_4da4b386_opt.jpg",
       "id": "61e7673ef6d774c7f0f50b4c",
       "teacher": "Patton Heath",
-      "name": "duis velit ex",
+      "name": "Pintura",
       "about": "Duis laboris non mollit dolor quis pariatur consequat qui.",
-      "price": 22.86,
+      "price": "S/. 22.86",
       "stars": 3
     },
     {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
+      "img": "https://www.cocinayvino.com/wp-content/uploads/2020/04/123864572_m-e1588006506154.jpg",
       "id": "61e7673e7aeb653b70e210c3",
       "teacher": "Chris Wilson",
-      "name": "do qui sint",
+      "name": "Cocina",
       "about": "Occaecat nostrud dolore sint deserunt officia sint nulla.",
-      "price": 38.35,
+      "price": "S/. 38.35",
       "stars": 5
     },
     {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
+      "img": "https://www.oficinaempleo.com/blog/wp-content/uploads/2017/08/cursos-electronica.jpg",
       "id": "61e7673e2d9619e9cc1605f3",
       "teacher": "Tonya Prince",
-      "name": "fugiat ullamco adipisicing",
+      "name": "Electronica",
       "about": "Ex ad ullamco consectetur amet deserunt labore id elit.",
-      "price": 35.14,
+      "price": "S/. 35.14",
       "stars": 2
     },
     {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
+      "img": "https://cadenaser00.epimg.net/emisora/imagenes/2017/09/06/ser_madrid_norte/1504692381_869624_1504692691_noticia_normal.jpg",
       "id": "61e7673e545caa4af5f6cebc",
       "teacher": "Shaw Mosley",
-      "name": "veniam consectetur ullamco",
+      "name": "Robotica",
       "about": "Nisi et non excepteur qui tempor aute.",
-      "price": 12.76,
+      "price": "S/. 12.76",
       "stars": 3
     },
     {
       "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
       "id": "61e7673e2b97656c1240e264",
       "teacher": "Donaldson Whitfield",
-      "name": "et commodo nulla",
+      "name": "Sistemas",
       "about": "Adipisicing culpa ipsum aliqua dolor aliquip minim sit anim qui.",
-      "price": 22.04,
+      "price": "S/. 22.04",
       "stars": 4
     },
     {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
+      "img": "https://www.superprof.co/blog/wp-content/uploads/2019/08/talleres-costura-700x467.jpg.webp",
       "id": "61e7673edcb8682ccaa4df73",
       "teacher": "Effie Rosales",
-      "name": "qui commodo fugiat",
+      "name": "Modisteria",
       "about": "Nulla irure eu mollit cillum velit quis aliqua ea id proident.",
-      "price": 29.71,
+      "price": "S/. 29.71",
       "stars": 4
     },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e2cbcf6871a252163",
-      "teacher": "Pansy Franco",
-      "name": "reprehenderit cillum id",
-      "about": "Exercitation eu adipisicing nisi anim Lorem.",
-      "price": 31.83,
-      "stars": 4
-    },
-    { 
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e94f7987a0450e9bf",
-      "teacher": "Yates Day",
-      "name": "commodo nostrud amet",
-      "about": "Enim amet laborum nostrud dolor.",
-      "price": 44.76,
-      "stars": 1
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e6c681d2cbc404582",
-      "teacher": "Paul Joyce",
-      "name": "aliqua eiusmod officia",
-      "about": "Voluptate amet proident deserunt ex adipisicing commodo.",
-      "price": 10.36,
-      "stars": 4
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e3be6ec2dbc32af3f",
-      "teacher": "Shaffer Robbins",
-      "name": "labore nostrud eu",
-      "about": "Lorem minim ea laborum anim adipisicing ea magna elit.",
-      "price": 39.61,
-      "stars": 3
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673eaebf8b49a4ea709b",
-      "teacher": "Neva Kirkland",
-      "name": "fugiat exercitation consequat",
-      "about": "Quis adipisicing magna aliqua sint sit anim laboris.",
-      "price": 42.98,
-      "stars": 5
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673ea7effdb331d6dfe2",
-      "teacher": "Coleman David",
-      "name": "in eu tempor",
-      "about": "Ex reprehenderit sint duis sit consectetur.",
-      "price": 21.85,
-      "stars": 1
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e6d5fcfcedb645f33",
-      "teacher": "Bruce Randall",
-      "name": "in ea sint",
-      "about": "Laboris labore irure eu duis ea reprehenderit laborum excepteur pariatur.",
-      "price": 23.77,
-      "stars": 2
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673ed2c8693f054148d3",
-      "teacher": "Hillary French",
-      "name": "occaecat aliquip exercitation",
-      "about": "Ex nisi incididunt elit cupidatat occaecat.",
-      "price": 11.13,
-      "stars": 4
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673ef1c7af9937d2a99f",
-      "teacher": "Bette Reese",
-      "name": "fugiat aliqua consectetur",
-      "about": "Lorem amet ex culpa mollit incididunt consequat sint tempor.",
-      "price": 43.72,
-      "stars": 4
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673eaf44703a9bdc89f6",
-      "teacher": "Louise Mclaughlin",
-      "name": "eiusmod id ut",
-      "about": "Laboris minim ad veniam voluptate nostrud incididunt anim.",
-      "price": 32.04,
-      "stars": 4
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e335b9dcfac05a8be",
-      "teacher": "Juliet Reid",
-      "name": "eiusmod quis minim",
-      "about": "Ex consequat laboris adipisicing amet voluptate eu fugiat.",
-      "price": 18.54,
-      "stars": 4
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673ed05cbb1a4081c71f",
-      "teacher": "Fleming Wilkinson",
-      "name": "sit Lorem qui",
-      "about": "Et nisi labore excepteur incididunt sit ipsum ea esse dolor.",
-      "price": 37.88,
-      "stars": 3
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e9b107e1ac5339fa0",
-      "teacher": "Henry Wilkerson",
-      "name": "irure in reprehenderit",
-      "about": "Consectetur culpa amet incididunt exercitation dolore velit.",
-      "price": 25.02,
-      "stars": 5
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673ed1a609bea00c8d75",
-      "teacher": "Tammi Chaney",
-      "name": "laboris adipisicing ut",
-      "about": "Cupidatat aliquip sit occaecat mollit officia irure voluptate amet sit.",
-      "price": 19.07,
-      "stars": 5
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e461adcc929bc44ef",
-      "teacher": "Stevenson Ross",
-      "name": "duis mollit mollit",
-      "about": "Aliquip laboris labore pariatur velit eu proident ipsum commodo velit id.",
-      "price": 13.72,
-      "stars": 2
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e5cccf11d786e5f45",
-      "teacher": "Hancock Turner",
-      "name": "officia do veniam",
-      "about": "Aliqua pariatur aliqua magna adipisicing voluptate laborum Lorem.",
-      "price": 29.91,
-      "stars": 3
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e74c1f1c49e3b8334",
-      "teacher": "Rhoda Gibson",
-      "name": "commodo elit id",
-      "about": "Commodo consectetur laboris dolor excepteur nisi consequat.",
-      "price": 12.89,
-      "stars": 2
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e4c55ee54aa91ab3b",
-      "teacher": "Patti Montoya",
-      "name": "consectetur irure sunt",
-      "about": "Lorem consequat labore dolore ullamco esse nulla incididunt enims.",
-      "price": 11.77,
-      "stars": 1
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e74c1f1c49e3b8334",
-      "teacher": "Rhoda Gibson",
-      "name": "commodo elit id",
-      "about": "Commodo consectetur laboris dolor excepteur nisi consequat.",
-      "price": 12.89,
-      "stars": 2
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmyLjORKt3NzRGN6b2r3NPs_jvMkh_Wa9Ep2Kt4ex27YpwakbMK5MeiWquDATLa0diyM&usqp=CAU",
-      "id": "61e7673e4c55ee54aa91ab3b",
-      "teacher": "Patti Montoya",
-      "name": "consectetur irure sunt",
-      "about": "Lorem consequat labore dolore ullamco esse nulla incididunt enims.",
-      "price": 11.77,
-      "stars": 1
-    }
+    
   ]
 
   export default courses_home;
