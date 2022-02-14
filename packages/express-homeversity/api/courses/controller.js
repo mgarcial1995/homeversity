cf5eee8bc5371a3c1a46570be9d414f1a5a89807
GIOVANNI MARTIN GARCIA LAGOS <Giovanni MartinGarcia Lagos@amertv.com.pe>
2022-02-13 19:50:49 -0500

    feat(express-homeversity): connect mongo database

--- packages/express-homeversity/api/courses/controller.js
+++ packages/express-homeversity/api/courses/controller.js
@@ -5,25 +5,54 @@ const db = lowdDB(adapter);
 const { nanoid } = require('nanoid');
 
 const CourseModel = require('./model')
+const mongoose = require("mongoose")
 
 exports.getAllCourses = (req, res) =>{
-    const courses = db.get('courses').value(); // query
-    res.status(200).json({ success: true, courses: courses })
+    // const courses = db.get('courses').value(); // query
+    CourseModel.find().exec().then(response =>{
+        res.status(200).json({ success: true, courses: response })
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json({ success: false, error: err})
+    })
 }
-exports.newCourse = (req, res) =>{
+exports.newCourse = async (req, res) =>{
     const body = req.body
-    // console.log(body)
-    const courseSchema = CourseModel.courseSchema
-    const result = courseSchema.validate(body);
-    const { value, error } = result;
-    if(error == null){
-        const course = {
-            _id: nanoid(),
-            ...value
+
+    const Courses = new CourseModel({
+        name: body.name,
+        description: body.description,
+        valoration: body.valoration,
+        teacher: body.teacher,
+        price: body.price,
+        photo: body.photo,
+        video: body.video,
+        hours: body.hours,
+        level: body.level,
+        category: body.category
+    });
+    await Courses.save()
+        .then(response =>{
+            console.log(response)
+            res.status(200).json({ success: true, message: 'Course has been created', data: response })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json({ success: false, error: err})
         }
-        db.get('courses').push(course).write();
-        res.status(200).json({ success: true, message: 'Course has been created', data: course })
-    }else{
-        res.status(400).json({ success: false, data: value, error: error, cosa: "Giantory" })
-    }
+    );
+    // console.log(body)
+    // const courseSchema = CourseModel.courseSchema
+    // const result = courseSchema.validate(body);
+    // const { value, error } = result;
+    // if(error == null){
+    //     const course = {
+    //         _id: nanoid(),
+    //         ...value
+    //     }
+    //     db.get('courses').push(course).write();
+    //     res.status(200).json({ success: true, message: 'Course has been created', data: course })
+    // }else{
+    //     res.status(400).json({ success: false, data: value, error: error, cosa: "Giantory" })
+    // }
 }
--- packages/express-homeversity/index.js
+++ packages/express-homeversity/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const lowdDB = require('lowdb');
-const FyleSync = require('lowdb/adapters/FileSync');
+// const lowdDB = require('lowdb');
+// const FyleSync = require('lowdb/adapters/FileSync');
 const cors = require('cors');
+const mongoose = require("mongoose");
 
 const coursesRoutes = require("./api/courses/routes");
 
@@ -9,12 +10,23 @@ const coursesRoutes = require("./api/courses/routes");
 const PORT = 3001;
 
 // Database Setup
-const adapter = new FyleSync('db.json');
-const db = lowdDB(adapter);
-db.defaults({ courses: [], programs:[] }).write();
+// const adapter = new FyleSync('db.json');
+// const db = lowdDB(adapter);
+// db.defaults({ courses: [], programs:[] }).write();
 
 // Express Setup
 const app = express();
+
+
+// MONGODBIMPORT
+mongoose.connect(
+	`mongodb://127.0.0.1:27017/homeversity`,
+	{ useUnifiedTopology: true, useNewUrlParser: true }
+);
+
+// mongoose.connect(
+// 	`mongodb+srv://admin:martin12345@cluster0.n075l.mongodb.net/homeversity?retryWrites=true&w=majority`
+// );
 
 // Global Middlewares
 app.use(express.json());
