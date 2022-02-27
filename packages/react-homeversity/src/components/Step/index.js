01031834f6cf12400c8890dbbce69143187d867b
Giantory <giantory.bryant@gmail.com>
2022-02-26 20:09:00 -0500

    feat(react-homeversity):add Step component

---
+++ packages/react-homeversity/src/components/Step/index.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { StepContext } from "../../views/Carshop";
+import "./styles.sass";
+
+const Step = (props) => {
+  const { currentStep } = useContext(StepContext);
+  return (
+    <div className={"step" + (currentStep===props.index+1 ? "-selected" : "")}>
+      <div className="step-circle">{props.index + 1}</div>
+      <div className="step-label">{props.step}</div>
+    </div>
+  );
+};
+
+export default Step;
